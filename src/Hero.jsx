4a6f1fb2e1854b864f026a34cca7a94b4d055871
Lawrence Caulker <locaulker@gmail.com>
2023-10-03 06:33:32 -0400

    end hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,16 @@
 const Hero = () => {
-  return <h2>Hero</h2>
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+      </div>
+      <p>
+        Strapi is the leading open-source headless CMS. It's 100% JavaScript and{" "}
+        <br /> fully customizable
+      </p>
+    </div>
+  )
 }
 export default Hero
