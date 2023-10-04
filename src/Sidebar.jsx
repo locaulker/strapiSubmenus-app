ae8cf9330765c6f8e5c8f045619cbe955909ce23
Lawrence Caulker <locaulker@gmail.com>
2023-10-04 01:11:30 -0400

    added sidebar structure mapped over sublinks and links

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,39 @@
+import { FaTimes } from "react-icons/fa"
+import { useGlobalContext } from "./Context"
+import sublinks from "./data"
+
 const Sidebar = () => {
-  return <h2>Sidebar</h2>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item) => {
+            const { links, page, pageId } = item
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { id, label, icon, url } = link
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </aside>
+  )
 }
 export default Sidebar
